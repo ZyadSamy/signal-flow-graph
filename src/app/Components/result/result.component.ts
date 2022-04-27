6bcedcc4d9fd833f8e2bc836b36c855696e8d4e9
Meniem Hany <es-abdelmoneim.hany2019@alexu.edu.eg>
2022-04-27 20:06:38 +0200

    Hopefully Final

--- src/app/Components/result/result.component.ts
+++ src/app/Components/result/result.component.ts
@@ -1,5 +1,7 @@
+import { ThisReceiver, ThrowStmt } from '@angular/compiler';
 import { Component, OnInit, Input } from '@angular/core';
 import { GraphService } from '../../Services/graph.service';
+import structuredClone from '@ungap/structured-clone';
 
 @Component({
   selector: 'result',
@@ -35,7 +37,34 @@ export class ResultComponent implements OnInit {
         .subscribe((loops) => (this.feedbackLoops = loops));
       this.graphService.getNonTouchingLoops().subscribe((nonTouchingLoops) => {
         this.nonTouchingLoops = nonTouchingLoops;
-        console.log(nonTouchingLoops);
+        let ntClone = structuredClone(this.nonTouchingLoops)
+        console.log(ntClone);
+        let i = 0;
+        console.log("Non Touching Loops Length: " + this.nonTouchingLoops.length.toString())
+        while(i < this.nonTouchingLoops.length ){
+          if(this.nonTouchingLoops[i].length == 0){
+            break;
+          }
+          i++;
+          console.log(i)
+        }
+        if(i == this.nonTouchingLoops.length){
+          //Do Nothing
+        }
+        else{
+          this.nonTouchingLoops.splice(i);
+        }
+        
+        console.log(this.nonTouchingLoops)
+
+        // this.nonTouchingLoops.forEach(element => {
+        //   if(element.length == 0){
+        //     console.log("array no:" + this.nonTouchingLoops.indexOf(element).toString() + " has length 0")
+        //     this.nonTouchingLoops.splice(this.nonTouchingLoops.indexOf(element), 1)
+        //     console.log("array no:" + this.nonTouchingLoops.indexOf(element).toString() + " should have been removed" )
+        //   }
+        // console.log(nonTouchingLoops);
+        // });
       });
     });
   }
